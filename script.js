87d5bd4d2721e6d5fe55f514c7b6b4963f971f8d
Hahlina <hahlina@e-macis-MacBook-Pro.local>
2023-01-20 00:07:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+
+import "./script/preloader.js"
+import "./script/burger.js"
+import "./script/carousel.js"
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+
+export default {
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "partials"),
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // discover: resolve(__dirname, 'nested/discover.html'),
+      
+      },
+    },
+  },
+};
